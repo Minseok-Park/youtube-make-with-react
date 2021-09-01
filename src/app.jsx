df6fbc2792d1af1f50f519b8b8acc14b55c1bc6b
minseok-Park <pms0905@naver.com>
2021-09-01 18:10:07 +0900

    유튜브 비디오 리스트 & 검색 & 채널 메소드 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,11 @@
 import styles from "./app.module.css";
 
-function App() {
-  return <h2>hi</h2>;
+function App({ youtube }) {
+  return (
+    <section className={styles.app}>
+      <h2>dd</h2>
+    </section>
+  );
 }
 
 export default App;
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,10 +1,13 @@
 class Youtube {
   constructor(httpClient) {
     this.youtube = httpClient;
+    this.videoList = [];
+    this.channels = {};
   }
 
-  mostPopular() {
-    const response = this.youtube.get("videos", {
+  // 유튜브 비디오 목록 리스트
+  async mostPopular() {
+    const response = await this.youtube.get("videos", {
       params: {
         part: "snippet",
         chart: "mostPopular",
@@ -13,8 +16,55 @@ class Youtube {
       },
     });
 
-    return response.data.items;
+    this.videoList = [];
+    response.data.items.map((video) => {
+      return this.videoList.push(this.channel(video.snippet.channelId, video));
+    });
+
+    return Promise.all(this.videoList).then((values) => values);
+  }
+
+  //유튜브 검색
+  async search(query) {
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        q: query,
+        maxResults: 28,
+        type: "video",
+      },
+    });
+
+    const newVideos = [];
+
+    this.videoList = [];
+
+    response.data.items.map((item) => {
+      return newVideos.push({ ...item, id: item.id.videoId });
+    });
+
+    newVideos.map((video) => {
+      return this.videoList.push(this.channel(video.snippet.channelId, video));
+    });
+
+    return Promise.all(this.videoList) //
+      .then((values) => values);
+  }
+
+  // 유튜브 채널
+  async channel(channelId, video) {
+    const response = await this.youtube.get("channels", {
+      params: {
+        part: "snippet, statistics",
+        id: channelId,
+      },
+    });
+
+    if (response.data.items[0].hasOwnProperty("snippet")) {
+      response.data.items[0].channelInfo = response.data.items[0].snippet;
+      delete response.data.items[0].snippet;
+    }
+
+    return (this.channel = { ...response.data.items[0], ...video });
   }
 }
-
-export default Youtube;
